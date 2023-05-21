48819e23f4a8f5d48fa4b3d4ac5782482a39513c
Deepak Thapa <32347879+dtsuper3@users.noreply.github.com>
2023-05-21 23:20:08 +0530

    fixed

--- App.tsx
+++ App.tsx
@@ -1,34 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import MainNavigation from "./src/navigation";
-import { Asset } from 'expo-asset';
-import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-
-  async function cacheResourcesAsync() {
-    const images = [require("./assets/splash.png")];
-
-    const cacheImages = images.map(image => {
-      return Asset.fromModule(image).downloadAsync();
-    });
-
-    Promise.all(cacheImages);
-  }
-
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={cacheResourcesAsync}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
-  }
-
   return (
     <MainNavigation />
   )
-
-
 }
